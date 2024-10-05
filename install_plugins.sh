e76bb10ccc0fd0432d947d9bc2d8d8b5015ed612
bass-tapper <4stringrebel@gmail.com>
2024-10-05 08:29:08 +0300

    install plugins script update

--- install_plugins.sh
+++ install_plugins.sh
@@ -2,9 +2,11 @@
 
 JENKINS_SERVER="http://192.168.39.132:8080"
 
-java -jar jenkins-cli.jar -s $JENKINS_SERVER install-plugin git pipeline-maven chucknorris ws-cleanup
+echo $JENKINS_SERVER
 
-java -jar jenkins-cli.jar -s $JENKINS_SERVER safe-restart
+#java -jar jenkins-cli.jar -s $JENKINS_SERVER install-plugin git pipeline-maven chucknorris ws-cleanup
+
+#java -jar jenkins-cli.jar -s $JENKINS_SERVER safe-restart
 
 
 
